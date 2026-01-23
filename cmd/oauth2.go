7168fc78becaf195abd1bddc35fd7a7170ea6ca0
Mateusz Bilski <mbilski@secureauth.com>
2026-01-23 15:08:10 +0100

    linter

--- cmd/oauth2.go
+++ cmd/oauth2.go
@@ -227,16 +227,16 @@ func (c *OAuth2Cmd) Authorize(
 		return c.DeviceGrantFlow(clientConfig, serverConfig, hc)
 	}
 
-	return fmt.Errorf("Unknown grant type: %s", clientConfig.GrantType)
+	return fmt.Errorf("unknown grant type: %s", clientConfig.GrantType)
 }
 
 func (c *OAuth2Cmd) PrintResult(result interface{}) {
 	output, err := json.Marshal(result)
 	if err != nil {
-		fmt.Fprintf(c.ErrOrStderr(), "%+v", err)
-		fmt.Fprintln(c.ErrOrStderr())
+		_, _ = fmt.Fprintf(c.ErrOrStderr(), "%+v", err)
+		_, _ = fmt.Fprintln(c.ErrOrStderr())
 		return
 	}
 
-	fmt.Fprintln(c.OutOrStdout(), string(output))
+	_, _ = fmt.Fprintln(c.OutOrStdout(), string(output))
 }
